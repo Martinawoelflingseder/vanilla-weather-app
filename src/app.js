bba00df8fe6973257aad133a08b1dbeb6553f048
Martinawoelflingseder <116192968+Martinawoelflingseder@users.noreply.github.com>
2022-12-04 12:20:19 +0100

    Showing current temperature

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElemnet = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElemnet.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "1fd8093fa5ff12d796d7de756cc9d6b9";
+let units = "metric";
+let city = "Graz";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+axios.get(apiUrl).then(showTemperature);
